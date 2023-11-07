079ee7b1b1bb576db1594b3b27fa43e5d6033050
Alan Rezende <alan.rezendeeee@hotmail.com>
2023-11-07 10:13:32 -0300

    ajustes arquivo

--- uninstall-gitlab-runner.sh
+++ uninstall-gitlab-runner.sh
@@ -1,30 +1,46 @@
-# Para o serviço Gilab Runner
-sudo gitlab-runner stop
+# Iniciar o log
+echo "Iniciando o processo de desinstalação do GitLab Runner..."
+
+# Para o serviço GitLab Runner
+echo "Parando o serviço GitLab Runner..." && \
+sudo gitlab-runner stop && echo "O serviço GitLab Runner foi parado com sucesso."
 
 # Desabilite o serviço para que ele não inicie na inicialização do sistema
-sudo gitlab-runner uninstall
+echo "Desabilitando o serviço GitLab Runner..." && \
+sudo gitlab-runner uninstall && echo "O serviço GitLab Runner foi desabilitado com sucesso."
 
 # Remova o pacote GitLab Runner
-sudo apt-get remove gitlab-runner
+echo "Removendo o pacote GitLab Runner..." && \
+sudo apt-get remove gitlab-runner -y && echo "O pacote GitLab Runner foi removido com sucesso."
 
 # Dependências que não são mais necessárias
-sudo apt-get autoremove
+echo "Removendo dependências desnecessárias..." && \
+sudo apt-get autoremove -y && echo "Dependências desnecessárias foram removidas com sucesso."
 
-# Remova quaisquer arquivos de configuração residuais:
-sudo rm -rf /etc/gitlab-runner
+# Remova quaisquer arquivos de configuração residuais
+echo "Removendo arquivos de configuração residuais..." && \
+sudo rm -rf /etc/gitlab-runner && echo "Arquivos de configuração residuais removidos com sucesso."
 
 # Remova o usuário e grupo 'gitlab-runner', se foram criados
-sudo deluser --remove-home gitlab-runner
-sudo delgroup gitlab-runner
+echo "Removendo o usuário e grupo 'gitlab-runner'..." && \
+sudo deluser --remove-home gitlab-runner && \
+sudo delgroup gitlab-runner && echo "Usuário e grupo 'gitlab-runner' removidos com sucesso."
 
 # Limpe quaisquer arquivos temporários ou de cache criados pelo GitLab Runner
-sudo find / -name "*gitlab-runner*" 2>/dev/null
+echo "Limpando arquivos temporários ou de cache..." && \
+sudo find / -name "*gitlab-runner*" -exec rm -rf {} + 2>/dev/null && echo "Arquivos temporários ou de cache limpos com sucesso."
 
 # Remova o repositório do GitLab Runner das listas de fontes do APT, se aplicável
-sudo rm /etc/apt/sources.list.d/runner_gitlab-runner.list
+echo "Removendo o repositório do GitLab Runner das listas de fontes do APT..." && \
+sudo rm /etc/apt/sources.list.d/runner_gitlab-runner.list && echo "Repositório do GitLab Runner removido com sucesso das listas de fontes do APT."
 
 # Limpe o cache do APT
-sudo apt-get clean
+echo "Limpando o cache do APT..." && \
+sudo apt-get clean && echo "Cache do APT limpo com sucesso."
 
 # Atualize a lista de pacotes disponíveis
-sudo apt-get update
+echo "Atualizando a lista de pacotes disponíveis..." && \
+sudo apt-get update && echo "Lista de pacotes disponíveis atualizada com sucesso."
+
+# Concluir o log
+echo "Processo de desinstalação do GitLab Runner concluído."
